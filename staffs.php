54a684e11afe19ad9df1c18c2cf7a28eb760fcaf
Eric Wong <erichswong1990@gmail.com>
2024-06-22 22:05:47 +0800

    add missing label tag and swap gender and age position

--- staffs.php
+++ staffs.php
@@ -44,6 +44,12 @@ include_once 'staffs_crud.php';
             </div>
           </div>
           <div class="form-group">
+            <label for="staffage" class="col-sm-3 control-label">Age</label>
+            <div class="col-sm-9">
+              <td><input type="number" name="age" min="0" max="100" step="1" class="form-control id="staffage" placeholder="Staff Age" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_age']; ?>" required></td>
+            </div>
+          </div>
+          <div class="form-group">
             <label for="staffgender" class="col-sm-3 control-label">Gender</label>
             <div class="col-sm-9">
               <div class="radio">
@@ -54,135 +60,129 @@ include_once 'staffs_crud.php';
               <div class="radio">
                 <label>
                   <input name="gender" type="radio" id="staffgender" value="Female" <?php if(isset($_GET['edit'])) if($editrow['fld_gender']=="Female") echo "checked"; ?>> Female
-                </div>
+                </label>
               </div>
             </div>
-            <div class="form-group">
-              <label for="staffage" class="col-sm-3 control-label">Age</label>
-              <div class="col-sm-9">
-                <td><input type="number" name="age" min="0" max="100" step="1" class="form-control id="staffage" placeholder="Staff Age" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_age']; ?>" required></td>
-              </div>
-            </div> 
-
-            <div class="form-group">
-              <label for="productname" class="col-sm-3 control-label">Position</label>
-              <div class="col-sm-9">
-                <input name="position" type="text" class="form-control id="staffposition" placeholder="Staff Position" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_position']; ?>" required> 
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="col-sm-offset-3 col-sm-9">
-                <?php if (isset($_GET['edit'])) { ?>
-                  <input type="hidden" name="oldsid" value="<?php echo $editrow['fld_staff_id']; ?>">
-                  <button class="btn btn-default" type="submit" name="update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Update</button>
-                <?php } else { ?>
-                  <button class="btn btn-default" type="submit" name="create"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Create</button>
-                <?php } ?>
-                <button class="btn btn-default" type="reset"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Clear</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-          <div class="page-header">
-            <h2>Staffs List</h2>
           </div>
-          <table class="table table-striped table-bordered">
+          <div class="form-group">
+            <label for="productname" class="col-sm-3 control-label">Position</label>
+            <div class="col-sm-9">
+              <input name="position" type="text" class="form-control id="staffposition" placeholder="Staff Position" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_position']; ?>" required> 
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-9">
+              <?php if (isset($_GET['edit'])) { ?>
+                <input type="hidden" name="oldsid" value="<?php echo $editrow['fld_staff_id']; ?>">
+                <button class="btn btn-default" type="submit" name="update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Update</button>
+              <?php } else { ?>
+                <button class="btn btn-default" type="submit" name="create"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Create</button>
+              <?php } ?>
+              <button class="btn btn-default" type="reset"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Clear</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+        <div class="page-header">
+          <h2>Staffs List</h2>
+        </div>
+        <table class="table table-striped table-bordered">
+          <tr>
+            <td>Staff ID</td>
+            <td>Staff Name</td>
+            <td>Gender</td>
+            <td>Age</td>
+            <td>Position</td>
+            <td></td>
+          </tr>
+          <?php
+      // Read
+          $per_page = 5;
+          if (isset($_GET["page"]))
+            $page = $_GET["page"];
+          else
+            $page = 1;
+          $start_from = ($page-1) * $per_page;
+          try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("select * from tbl_staffs_a192212_pt2 LIMIT $start_from, $per_page");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+          }
+          catch(PDOException $e){
+            echo "Error: " . $e->getMessage();
+          }
+          foreach($result as $readrow) {
+            ?>
             <tr>
-              <td>Staff ID</td>
-              <td>Staff Name</td>
-              <td>Gender</td>
-              <td>Age</td>
-              <td>Position</td>
-              <td></td>
+              <td><?php echo $readrow['fld_staff_id']; ?></td>
+              <td><?php echo $readrow['fld_staff_name']; ?></td>
+              <td><?php echo $readrow['fld_gender']; ?></td>
+              <td><?php echo $readrow['fld_age']; ?></td>
+              <td><?php echo $readrow['fld_position']; ?></td>
+              <td>
+                <a href="staffs.php?pid=<?php echo $readrow['fld_staff_id']; ?>" class="btn btn-warning btn-xs" role="button">Details</a>
+                <a href="staffs.php?edit=<?php echo $readrow['fld_staff_id']; ?>" class="btn btn-success btn-xs" role="button"> Edit </a>
+                <a href="staffs.php?delete=<?php echo $readrow['fld_staff_id']; ?>" onclick="return confirm('Are you sure to delete?');" class="btn btn-danger btn-xs" role="button">Delete</a>
+              </td>
             </tr>
             <?php
-      // Read
-            $per_page = 5;
-            if (isset($_GET["page"]))
-              $page = $_GET["page"];
-            else
-              $page = 1;
-            $start_from = ($page-1) * $per_page;
+          }
+          $conn = null;
+          ?>
+        </table>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+        <nav>
+          <ul class="pagination">
+            <?php
             try {
               $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
               $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $stmt = $conn->prepare("select * from tbl_staffs_a192212_pt2 LIMIT $start_from, $per_page");
+              $stmt = $conn->prepare("SELECT * FROM tbl_staffs_a192212_pt2");
               $stmt->execute();
               $result = $stmt->fetchAll();
+              $total_records = count($result);
             }
             catch(PDOException $e){
               echo "Error: " . $e->getMessage();
             }
-            foreach($result as $readrow) {
-              ?>
-              <tr>
-                <td><?php echo $readrow['fld_staff_id']; ?></td>
-                <td><?php echo $readrow['fld_staff_name']; ?></td>
-                <td><?php echo $readrow['fld_gender']; ?></td>
-                <td><?php echo $readrow['fld_age']; ?></td>
-                <td><?php echo $readrow['fld_position']; ?></td>
-                <td>
-                  <a href="staffs.php?pid=<?php echo $readrow['fld_staff_id']; ?>" class="btn btn-warning btn-xs" role="button">Details</a>
-                  <a href="staffs.php?edit=<?php echo $readrow['fld_staff_id']; ?>" class="btn btn-success btn-xs" role="button"> Edit </a>
-                  <a href="staffs.php?delete=<?php echo $readrow['fld_staff_id']; ?>" onclick="return confirm('Are you sure to delete?');" class="btn btn-danger btn-xs" role="button">Delete</a>
-                </td>
-              </tr>
+            $total_pages = ceil($total_records / $per_page);
+            ?>
+            <?php if ($page==1) { ?>
+              <li class="disabled"><span aria-hidden="true">«</span></li>
+            <?php } else { ?>
+              <li><a href="staffs.php?page=<?php echo $page-1 ?>" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
               <?php
             }
-            $conn = null;
-            ?>
-          </table>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-          <nav>
-            <ul class="pagination">
-              <?php
-              try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stmt = $conn->prepare("SELECT * FROM tbl_staffs_a192212_pt2");
-                $stmt->execute();
-                $result = $stmt->fetchAll();
-                $total_records = count($result);
-              }
-              catch(PDOException $e){
-                echo "Error: " . $e->getMessage();
-              }
-              $total_pages = ceil($total_records / $per_page);
+            for ($i=1; $i<=$total_pages; $i++)
+              if ($i == $page)
+                echo "<li class=\"active\"><a href=\"staffs.php?page=$i\">$i</a></li>";
+              else
+                echo "<li><a href=\"staffs.php?page=$i\">$i</a></li>";
               ?>
-              <?php if ($page==1) { ?>
-                <li class="disabled"><span aria-hidden="true">«</span></li>
+              <?php if ($page==$total_pages) { ?>
+                <li class="disabled"><span aria-hidden="true">»</span></li>
               <?php } else { ?>
-                <li><a href="staffs.php?page=<?php echo $page-1 ?>" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
-                <?php
-              }
-              for ($i=1; $i<=$total_pages; $i++)
-                if ($i == $page)
-                  echo "<li class=\"active\"><a href=\"staffs.php?page=$i\">$i</a></li>";
-                else
-                  echo "<li><a href=\"staffs.php?page=$i\">$i</a></li>";
-                ?>
-                <?php if ($page==$total_pages) { ?>
-                  <li class="disabled"><span aria-hidden="true">»</span></li>
-                <?php } else { ?>
-                  <li><a href="staffs.php?page=<?php echo $page+1 ?>" aria-label="Previous"><span aria-hidden="true">»</span></a></li>
-                <?php } ?>
-              </ul>
-            </nav>
-          </div>
+                <li><a href="staffs.php?page=<?php echo $page+1 ?>" aria-label="Previous"><span aria-hidden="true">»</span></a></li>
+              <?php } ?>
+            </ul>
+          </nav>
         </div>
       </div>
-      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-      <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script src="js/bootstrap.min.js"></script>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
-    </body>
-    </html>
+  </body>
+  </html>
