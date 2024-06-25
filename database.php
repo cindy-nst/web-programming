9ca35fc842e6ac5ddc3d11563eea8b1cbda0e39e
SING TI NG <ngsingti1654@gmail.com>
2024-06-25 22:56:16 +0800

    Amend database credential.

--- database.php
+++ database.php
@@ -1,13 +1,8 @@
 <?php
  
-// $servername = "lrgs.ftsm.ukm.my";
-// $username = "a192212";
-// $password = "littlepurplelion";
-// $dbname = "a192212";
-
-$servername = "127.0.0.1";
-$username = "root";
-$password = "nopassword";
-$dbname = "a192212";
+ $servername = "lrgs.ftsm.ukm.my";
+ $username = "a192212";
+ $password = "littlepurplelion";
+ $dbname = "a192212";
  
 ?>
